14ef976188306548285ef1ace542d249353489ef
johnformata <johnvianaluz@gmail.com>
2024-04-13 13:10:23 -0300

    feat: projeto inicio

---
+++ apploja/apploja/src/main/java/com/lojavirtual/apploja/ApplojaApplication.java
@@ -0,0 +1,13 @@
+package com.lojavirtual.apploja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplojaApplication.class, args);
+	}
+
+}
---
+++ apploja/apploja/src/test/java/com/lojavirtual/apploja/ApplojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojavirtual.apploja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
